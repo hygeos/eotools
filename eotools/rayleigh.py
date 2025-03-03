97f4dcf9cdb112dd35056978d8a623d55df29527
François Steinmetz <fs@hygeos.com>
2025-03-03 09:45:12 +0100

    Add rayleigh module
    Initially developed in geo-oc

---
+++ eotools/rayleigh.py
@@ -0,0 +1,154 @@
+'''
+ Copyright (c) 2024 EUMETSAT
+ License: MIT
+'''
+
+from pathlib import Path
+from typing import Literal
+import numpy as np
+import xarray as xr
+from eotools.srf import integrate_srf
+from eotools.bodhaine import rod
+from eotools.chapman import chapman
+
+from core.interpolate import interp, Linear
+
+
+
+def load_rayleigh_lut(lut_rayleigh_pp: Path,
+                      lut_rayleigh_sp: Path,
+                      rayleigh: Literal["PP", "SP"],
+                      **kwargs) -> xr.Dataset:
+    """Load the Rayleigh LUT (PP or SP)
+    
+    Relative azimuth angle convention: standard (raa = saa - vaa)
+
+    Returns a dataset with:
+        - reflectance_glitter (reflectance with a rough sea surface, no atmosphere)
+        - reflectance_toa (including glitter)
+        - T_tot_down: total transmission (downward)
+        - T_tot_up: total transmission (upward)
+    """
+    lut = xr.Dataset()
+
+    PP = xr.open_dataset(lut_rayleigh_pp,
+                            chunks=-1)
+
+    # set standard convention, raa = saa - vaa
+    PP = PP.assign_coords(raa=180 - PP.raa)
+
+    if rayleigh == 'PP':
+
+        lut['reflectance_glitter'] = PP['reflectance_glitter']
+        lut['reflectance_toa'] = PP['reflectance_toa']
+        lut['T_tot_down'] = PP['trans_flux_down']
+        lut['T_tot_up'] = PP['trans_flux_up']
+
+    elif rayleigh == 'SP':
+
+        SP = xr.open_dataset(lut_rayleigh_sp,
+                             chunks=-1)
+
+        SP = SP.rename({'mus_s': 'mu_s', 'mus_v': 'mu_v'})
+
+        # set standard convention, raa = saa - vaa
+        SP = SP.assign_coords(raa=180 - SP.raa)
+
+        lut['reflectance_glitter'] = SP['rho_total'] - SP['rho_path']
+        lut['reflectance_toa'] = SP['rho_total']
+
+        # Apply spherical correction to the direct term of the PP transmission
+        lut["T_tot_down"] = (
+            PP["trans_flux_down"]
+            - np.exp(-PP.odr * (1 / PP.mu_s))
+            + np.exp(-PP.odr * chapman(PP.mu_s))
+        ).interp(odr=SP.odr, mu_s=SP.mu_s)
+        lut["T_tot_up"] = (
+            PP["trans_flux_up"]
+            - np.exp(-PP.odr * (1 / PP.mu_v))
+            + np.exp(-PP.odr * chapman(PP.mu_v))
+        ).interp(odr=SP.odr, mu_v=SP.mu_v)
+    
+    else:
+        raise ValueError(rayleigh)
+    
+    return lut.compute(scheduler='sync')
+
+
+def rayleigh_correction(ds: xr.Dataset, srf: xr.Dataset, **cfg):
+    """Apply Rayleigh correction to `ds`
+    
+    This creates the following variables in `ds`:
+
+    +-------------+-----------------------------------------------+
+    | Variable    | Description                                   |
+    +=============+===============================================+
+    | rho_rc      | Rayleigh-corrected signal                     |
+    +-------------+-----------------------------------------------+
+    | rho_mol_gli | Rayleigh + glint reflectance                  |
+    +-------------+-----------------------------------------------+
+    | rho_mod     | Rayleigh reflectance (no glint)               |
+    +-------------+-----------------------------------------------+
+    | t_d         | Total diffuse transmittance (downward+upward) |
+    +-------------+-----------------------------------------------+
+
+    Parameters
+    ----------
+    ds : xr.Dataset
+        The Dataset used for inputs/outputs.
+    srf : xr.Dataset
+        The sensor spectral response function (SRF).
+    """
+
+    lut = load_rayleigh_lut(**cfg)
+
+    if "odr" in ds:
+        odr = ds["odr"]
+        # TODO: account for sea level pressure
+        attrs = {"rod_source": str(odr.values)}
+    else:
+        odr_dict = integrate_srf(srf, lambda wav_nm: rod(wav_nm / 1000))
+        odr = xr.DataArray([odr_dict[b] for b in ds.bands.values], dims="bands")
+        attrs = {"rod_source": "Bodhaine99"}
+
+    wdspd = ds["horizontal_wind"]
+
+    coords = {
+        "raa": Linear(ds.raa),
+        "mu_s": Linear(ds.mus, bounds='nan'),
+        "mu_v": Linear(ds.muv, bounds='nan'),
+        "wdspd": Linear(wdspd, bounds='clip'),
+        "odr": Linear(odr),
+    }
+
+    rho_toa = lut.reflectance_toa
+    rho_gli = lut.reflectance_glitter
+    ds["rho_mol_gli"] = interp(
+        rho_toa,
+        **coords,
+    )
+    ds["rho_mol_gli"].attrs.update(
+        {"desc": "Rayleigh + sun glint reflectance", **attrs}
+    )
+
+    ds["rho_mol"] = interp(
+        rho_toa - rho_gli,
+        **coords,
+    )
+    ds["rho_mol"].attrs.update({"desc": "Rayleigh reflectance (no sun glint)", **attrs})
+
+    ds["rho_rc"] = ds["rho_gc"] - ds["rho_mol_gli"]
+    ds["rho_rc"].attrs.update({"desc": "Rayleigh corrected reflectance", **attrs})
+
+    # Total atmospheric diffuse transmittance
+    ds["t_d"] = interp(
+        lut.T_tot_down,
+        mu_s=Linear(ds.mus, bounds="nan"),
+        odr=Linear(odr),
+        wdspd=Linear(wdspd, bounds="clip"),
+    ) * interp(
+        lut.T_tot_up,
+        mu_v=Linear(ds.mus, bounds="nan"),
+        odr=Linear(odr),
+    )
+    ds["t_d"].attrs.update({"desc": "Total atmospheric transmittance", **attrs})
