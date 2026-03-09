31589ca506b25e91ffd2ab562535cfd5a0fb774c
François Steinmetz <fs@hygeos.com>
2026-03-09 15:59:06 +0100

    units.py: add check_units

--- eotools/units.py
+++ eotools/units.py
@@ -6,6 +6,25 @@ from cf_xarray.units import units
 units.define("Dobson = 2.1415E-05 kg/m**2")
 
 
+def check_units(da: xr.DataArray, expected_units: str):
+    """
+    Validate that a DataArray has the expected units.
+
+    Parameters
+    ----------
+    da : xr.DataArray
+        The DataArray to check for units.
+    expected_units : str
+        The expected units string (e.g., 'Dobson', 'kg/m**2').
+    """
+    current_unit = da.attrs.get("units")
+    if not current_unit:
+        raise ValueError(f'No units defined for {da.name}')
+    if units(current_unit) != units(expected_units):
+        raise ValueError(
+            f"Wrong units detected for {da.name}. Expected {expected_units}, found {current_unit}."
+        )
+
 def convert(da: xr.DataArray, new_units: str) -> xr.DataArray:
     """
     Use pint-xarray for units conversion
